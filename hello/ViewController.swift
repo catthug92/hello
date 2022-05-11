9aad3c814af81bdd392b0537a7f3b7afa2e62f08
Artem78Nik <itnikitins@gmail.com>
2022-05-11 15:06:37 +0300

    связал код и сториборд

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var helloworldlabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // по умолчанию скрыли надпись
+        helloworldlabel.isHidden = true
+        // закруглили кнопку
+        toggleButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func makeButtonAction() {
+        // пишем логику для кнопки
+        if helloworldlabel.isHidden {
+            helloworldlabel.isHidden = false
+            //меняем текст кнопки
+            toggleButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloworldlabel.isHidden = true
+            toggleButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 }
 
